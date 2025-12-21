308d9bac8e97f6a3b73ee8c8ce196e0088656ce4
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-12-21 16:35:08 +0500

    fix: refactor user clock update mechanism for improved accuracy

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const _userClock = document.querySelector("#user-clock>.analog-clock");
 
-const mili = updateUserClock();
+function userTick() {
+  const mili = updateUserClock();
+  const delay = 1000 - mili;
+  setTimeout(tick, delay);
+}
 
-setInterval(() => {
-  updateUserClock();
-}, 1000 - mili);
+userTick();
 
 function updateUserClock() {
   const date = new Date(),
@@ -12,8 +14,11 @@ function updateUserClock() {
     mins = date.getMinutes(),
     sec = date.getSeconds(),
     mili = date.getMilliseconds();
-  _userClock.style.setProperty("--hrs-angle", `${30 * hrs + mins / 2}deg`);
-  _userClock.style.setProperty("--mins-angle", `${6 * mins}deg`);
+  _userClock.style.setProperty(
+    "--hrs-angle",
+    `${30 * (hrs % 12) + mins * 0.5 + sec * (0.5 / 60)}deg`
+  );
+  _userClock.style.setProperty("--mins-angle", `${6 * mins + sec * 0.1}deg`);
   _userClock.style.setProperty("--sec-angle", `${6 * sec}deg`);
   return mili;
 }
